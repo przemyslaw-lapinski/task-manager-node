4d6af42af7f388cd882070744ba085a1659de2fa
Przemyslaw Lapinski <przemyslaw.lapinski@hotmail.com>
2025-09-02 13:11:06 +0200

    chore(dev-env): fix problem with permissions and WSL, add config module (support .env)

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+interface EnvironmentVariables {
+    APP_NAME: string;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+    constructor(private configService: ConfigService<EnvironmentVariables>) {}
+
+    getHello(): string {
+    return 'Hello World! App: ' + this.configService.get<string>('APP_NAME');
   }
 }
